6937d1dfe44e3f7e3c75cc7cc1bd0c85e6eb9d8f
natdlc <nathanelcorpuz@gmail.com>
2023-10-03 21:09:48 -0600

    key prop fix

--- components/common/Select.js
+++ components/common/Select.js
@@ -4,7 +4,9 @@ export default function Select({ label, name, options }) {
 			<label for={name}>{label}</label>
 			<select className="border rounded p-2 bg-slate-50" name={name} id={name}>
 				{options.map((option) => (
-					<option value={option.value}>{option.label}</option>
+					<option key={option.value} value={option.value}>
+						{option.label}
+					</option>
 				))}
 			</select>
 		</div>
